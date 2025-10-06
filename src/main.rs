012f4f3104d9f1f984e9f4fa22ca333e3d79929d
Hank Memphis <harrier@vnpt.vn>
2025-10-07 06:13:37 +0700

    Initial project commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/*Goal: Write a program that manages employees in different departments of a company.
+You will use a HashMap to store the departments and the employees within them. */
+
+
+use std::io;
+use std::collections::HashMap;
+
+let mut company_employees: HashMap<String, Vec<String>> = HashMap::new();
+
+fn main() {
+}
